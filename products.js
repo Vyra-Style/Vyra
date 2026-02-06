8127315c86bf5c65408bcb974eb97786970cc100
Vyra-Style <saifmohmed261@gmail.com>
2026-02-06 20:46:24 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    name: "Elegant Summer Dress",
+    price: 199,
+    image: "https://i.imgur.com/abcd123.jpg",
+    paymentLink: "YOUR_PAYMENT_LINK"
+  },
+  {
+    name: "Chic Evening Gown",
+    price: 249,
+    image: "https://i.imgur.com/efgh456.jpg",
+    paymentLink: "YOUR_PAYMENT_LINK"
+  },
+  {
+    name: "Casual Daily Outfit",
+    price: 149,
+    image: "https://i.imgur.com/ijkl789.jpg",
+    paymentLink: "YOUR_PAYMENT_LINK"
+  }
+];
